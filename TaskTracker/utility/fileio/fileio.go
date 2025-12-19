76370b3a54f3e7f780634d8ead7d4cc279e38379
kaushik Gupta <kaushikmak35@gmail.com>
2025-12-19 17:57:47 +0530

    [TaskTracker|feat] added functionality to append,write data to disk

--- TaskTracker/utility/fileio/fileio.go
+++ TaskTracker/utility/fileio/fileio.go
@@ -1,37 +1,77 @@
 package fileio
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
-// function to get file path of data.json
+// get file path of data.json
 func getDataFilePath() (string, error) {
 	exePath, err := os.Executable()
 	if err != nil {
 		return "", err
 	}
-
 	exeDir := filepath.Dir(exePath)
-
-	dataPath := filepath.Join(exeDir, "data", "data.json")
-
-	return dataPath, nil
+	return filepath.Join(exeDir, "data", "data.json"), nil
 }
 
-// function to read an existing file
+// ReadData from file if not exits then create one
 func ReadData() ([]byte, error) {
 	path, err := getDataFilePath()
 	if err != nil {
 		return nil, err
 	}
+	// Create file if it doesn't exist
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		// Initialize with empty JSON array
+		err := os.WriteFile(path, []byte("[]"), 0644)
+		if err != nil {
+			return nil, err
+		}
+		return []byte("[]"), nil
+	}
 	return os.ReadFile(path)
 }
 
-// function to append data to file
-// func AppendFile() ([]byte, error){
+// WriteData overwrites the file with new data
+func WriteData(data []byte) error {
+	path, err := getDataFilePath()
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(path, data, 0644)
+}
 
-// }
+// AppendTask adds a serialized JSON object string to the JSON array in the file
+func AppendTask(taskJSON string) error {
+	// 1. Read existing data
+	data, err := ReadData()
+	if err != nil {
+		return err
+	}
 
-// function to delete file
-// func DeleteFile()
+	content := string(data)
+	content = strings.TrimSpace(content)
+
+	// 2. Handle empty file or empty list case
+	if len(content) == 0 || content == "[]" {
+		newContent := fmt.Sprintf("[%s]", taskJSON)
+		return WriteData([]byte(newContent))
+	}
+
+	// 3. Validation: Ensure we are appending to a valid list
+	if !strings.HasSuffix(content, "]") {
+		return fmt.Errorf("file content is not a valid JSON array")
+	}
+
+	// 4. Modify String:
+	// Remove the last ']'
+	content = content[:len(content)-1]
+	// Append ", " + newObject + "]"
+	newContent := fmt.Sprintf("%s, %s]", content, taskJSON)
+
+	// 5. Write back to file
+	return WriteData([]byte(newContent))
+}
