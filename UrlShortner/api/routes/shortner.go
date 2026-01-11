13214810186ff3c882ebb2e23b1a50b34509d7d9
kaushikmak <kaushikmak35@gmail.com>
2026-01-11 12:47:05 +0530

    IP based rate limiting

--- UrlShortner/api/routes/shortner.go
+++ UrlShortner/api/routes/shortner.go
@@ -1,11 +1,15 @@
 package routes
 
 import (
+	"os"
+	"strconv"
 	"time"
 
+	"github.com/Kaushikmak/UrlShortner/db"
 	"github.com/Kaushikmak/UrlShortner/helper"
 	"github.com/asaskevich/govalidator"
 	"github.com/gofiber/fiber/v3"
+	"github.com/redis/go-redis/v9"
 )
 
 type request struct {
@@ -27,6 +31,25 @@ func ShortnerURL(c fiber.Ctx) error {
 
 	if err := c.Bind().JSON(body); err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Cannot parse JSON"})
+	}
+
+	// rate limiting
+	redisDB_1 := db.CreateClient(1)
+	defer redisDB_1.Close()
+	val, err := redisDB_1.Get(db.Ctx, c.IP()).Result()
+	if err == redis.Nil {
+		_ = redisDB_1.Set(db.Ctx, c.IP(), os.Getenv("API_QUOTA"), 30*60*time.Second).Err()
+	} else {
+		valInt, _ := strconv.Atoi(val)
+		if valInt <= 0 {
+			limit, _ := redisDB_1.TTL(db.Ctx, c.IP()).Result()
+			return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+				"error":            "Rate limit exceeded",
+				"rate_limit_reset": limit / time.Nanosecond / time.Second,
+			})
+		}
+		redisDB_1.Decr(db.Ctx, c.IP())
+
 	}
 
 	// validate url
